1a5d4fc80780296522fbfe4d38021382770865ad
balazs4 <balazs4web@gmail.com>
2015-12-05 14:50:20 +0100

    Semi-auto extracting

--- extract.js
+++ extract.js
@@ -5,11 +5,38 @@ page.onConsoleMessage = function (msg, line, source) {
 	console.log("console>" + msg);
 }
 
+page.onResourceRequested = function (req, net) {
+	if (req.url.match(/cdn.rtl.hu/)){
+		net.abort();
+	}
+	//console.log("Request:" + req.url);
+};
 
-page.open("http://rtl.hu/most/budapest/ejjel-nappal-budapest-2015-12-02", function (status) {
+page.onResourceReceived = function(res){
+	//console.log("Response:" + res.url);
+}
 
-	if (status === "success") {
+phantom.onError = function (msg, trace) {
+	console.log("error> " + msg);
+};
+
+page.open("http://rtl.hu/rtlklub/budapest/videok", function (status) {
+	if (status != "success")
+		phantom.exit();
+
+
+	var link = page.evaluate(function () {
+		console.log("Getting link...");
+		return $(location).attr('origin') + "/" + $("a[href^='/most/budapest/ejjel-nappal-budapest-']").attr('href');
+	});
+
+	page.open(link, function (status) {
+		if (status != "success")
+			phantom.exit();
+
+
 		page.evaluate(function () {
+			console.log("Login...")
 			$("#menu-login").click();
 			$("input#logindarkform-email").val('varga27@gmail.com');
 			$("input#logindarkform-password").val('lofasz');
@@ -18,17 +45,23 @@ page.open("http://rtl.hu/most/budapest/ejjel-nappal-budapest-2015-12-02", functi
 
 		window.setTimeout(function () {
 			page.evaluate(function () {
+				console.log("Extract...")
 				var episode = {
 					"name": $("title").text(),
 					"url": $("video source").attr('src'),
 					"icon": $("video").attr('poster'),
 					"tags": ["ejjel-nappal-budapest", "rtlmost"],
-					"src" : $(location).attr('href')
+					"src": $(location).attr('href')
 				}
-
 				console.log(JSON.stringify(episode));
 			});
-			phantom.exit();
-		}, 5000);
-	}
+		}, 15000);
+	});
+
+
+
+
+	window.setTimeout(function () {
+		phantom.exit();
+	}, 30000);
 });
