5569740a8ba2b4855c2f2841df1743a462e3e5b5
balazs4 <balazs4web@gmail.com>
2015-12-06 17:08:35 +0100

    simplify extract scripts

--- ext/ejjel-nappal.js
+++ ext/ejjel-nappal.js
@@ -1,3 +1,14 @@
+var system = require('system');
+var args = system.args;
+
+var now = new Date();
+var month = now.getMonth() + 1;
+var day = now.getDate() < 10 ? "0" +now.getDate() : now.getDate();
+
+var date = args[1] || now.getFullYear() + "-" + month  + "-" + day;
+//console.log(date);
+
+
 var page = require('webpage').create();
 
 
@@ -6,56 +17,55 @@ page.onConsoleMessage = function (msg, line, source) {
 }
 
 page.onResourceRequested = function (req, net) {
-	if (req.url.match(/cdn.rtl.hu/)){ //let phantomjs ingore the images...
+	if (req.url.match(/cdn.rtl.hu/)) { //let phantomjs ingore the images...
 		net.abort();
 	}
+};
+
+
+page.onResourceReceived = function(res) {
+  if (res.stage === 'end') {
+    //console.log('Status code: ' + res.status);
+	if (res.status == 404)
+		phantom.exit();
+  }
 };
 
 phantom.onError = function (msg, trace) {
 	//console.log("error> " + msg);
 };
 
-page.open("http://rtl.hu/rtlklub/budapest/videok", function (status) {
+page.open("http://rtl.hu/most/budapest/ejjel-nappal-budapest-" + date, function (status) {
 	if (status != "success")
 		phantom.exit();
 
-
-	var link = page.evaluate(function () {
-		console.log("Getting link...");
-		return $(location).attr('origin') + "/" + $("a[href^='/most/budapest/ejjel-nappal-budapest-20']").attr('href');
-	});
-
-	page.open(link, function (status) {
-		if (status != "success")
-			phantom.exit();
-
-		page.evaluate(function () {
-			console.log("Login...")
-			$("#menu-login").click();
-			$("input#logindarkform-email").val('17kifli@gmail.com');
-			$("input#logindarkform-password").val('*****');
-			$("#login-dark-form button.login-button").click();
-			console.log("Waiting...");
-		});
-
-		window.setTimeout(function () {
-			var ep = page.evaluate(function () {
-				console.log("Extract...")
-				var episode = {
-					"name": $("title").text(),
-					"url": $("video source").attr('src'),
-					"icon": $("video").attr('poster'),
-					"tags": ["ejjel-nappal-budapest", "rtlmost"],
-					"src": $(location).attr('href')
-				}
-				return episode;
-			});
-			console.log(JSON.stringify(ep));
-			phantom.exit();
-		}, 7000);
-	});
 	
-	window.setTimeout(function(){
+	page.evaluate(function () {
+		console.log("Login...")
+		$("#menu-login").click();
+		$("input#logindarkform-email").val('17kifli@gmail.com');
+		$("input#logindarkform-password").val('*****');
+		$("#login-dark-form button.login-button").click();
+		console.log("Waiting...");
+	});
+
+	window.setTimeout(function () {
+		var ep = page.evaluate(function () {
+			console.log("Extract...")
+			var episode = {
+				"name": $("title").text(),
+				"url": $("video source").attr('src'),
+				"icon": $("video").attr('poster'),
+				"tags": ["ejjel-nappal-budapest", "rtlmost"],
+				"src": $(location).attr('href')
+			}
+			return episode;
+		});
+		console.log(JSON.stringify(ep));
+		phantom.exit();
+	}, 7000);
+
+	window.setTimeout(function () {
 		phantom.exit();
 	}, 30000);
 });
--- ext/index.sh
+++ ext/index.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 dir=$(dirname "$0")
-episode=$(phantomjs $dir/ejjel-nappal.js)
+episode=$(phantomjs $dir/ejjel-nappal.js $1)
 name="{"$(echo $episode | grep -Po '"name":.*?[^\\]"')"}"
 echo $episode
 echo $name
 
-mongo mediabag --eval "if (db.media.count($name) == 0) { db.media.insert($episode); }"	
+mongo piserver/mediabag --eval "if (db.media.count($name) == 0) { db.media.insert($episode); }"	
