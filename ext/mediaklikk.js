1e1b216f2b18b3949425765fdd7d7c39ce7c5b95
balazs4 <balazs4web@gmail.com>
2015-12-13 17:32:26 +0100

    Add '/media' POST

---
+++ ext/mediaklikk.js
@@ -0,0 +1,19 @@
+var item = {};
+
+item['name'] = jQuery("div.newStreamPlayerContainer.programInfoBox.nowPlayed span[itemprop='name']").text();
+item['tags'] = ['munkaugyek', 'mediaklikk'];
+item['icon'] = "https://upload.wikimedia.org/wikipedia/hu/f/f6/Munka%C3%BCgyek.jpg";
+item['src'] = window.location.href;
+item['extracted'] = new Date();
+
+jQuery.get(
+	jQuery("#hirado_player_box iframe")
+		.contents()
+		.find("iframe")
+		.attr('src'))
+	.then(function (content) {
+		var url = content.match(/{ 'file': '((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\.\/\?\:@\-_=&#])*' }/gm)[0].replace("'file': '", "");
+		item['url'] = url;
+		console.log(item);
+		jQuery.post("http://localhost:5555/media", item);
+	})
