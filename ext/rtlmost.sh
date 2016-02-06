ad2adf09b084d9374341ea23b9936146f346dee9
balazs4 <balazs4web@gmail.com>
2016-02-06 12:12:22 +0100

    Add url check to rtlmost extractor

--- ext/rtlmost.sh
+++ ext/rtlmost.sh
@@ -2,7 +2,8 @@
 dir=$(dirname "$0")
 episode=$(phantomjs $dir/rtlmost.js $1 $2)
 name="{"$(echo $episode | grep -Po '"name":.*?[^\\]"')"}"
+url=$(echo $episode | grep -Po '"url":.*?[^\\]"')
 echo $episode
 echo $name
 
-mongo piserver/mediabag --eval "if (db.media.count($name) == 0) { db.media.insert($episode); } else { db.media.update($name, $episode) }"
+[ ! -z $url ] && mongo piserver/mediabag --eval "if (db.media.count($name) == 0) { db.media.insert($episode); }"
